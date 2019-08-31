f5e4617b7b9520702924ee35552a8187fcf14434
cbonilla <christian.bonilla01@gmail.com>
2019-08-31 15:36:34 -0700

    Initial commit.

---
+++ script.js
@@ -0,0 +1,26 @@
+function snapshot() {
+	html2canvas(document.querySelector('#capture'), { backgroundColor: "null" }).then(function(canvas) {
+        var caption = document.querySelector(".text").innerText;
+		saveAs(canvas.toDataURL(), caption + '.png');
+	});
+}
+
+function saveAs(uri, filename) {
+	var link = document.createElement('a');
+
+	if (typeof link.download === 'string') {
+		link.href = uri;
+		link.download = filename;
+
+		//Firefox requires the link to be in the body
+		document.body.appendChild(link);
+
+		//simulate click
+		link.click();
+
+		//remove the link when done
+		document.body.removeChild(link);
+	} else {
+		window.open(uri);
+	}
+}
